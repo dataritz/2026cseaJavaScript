be0d8e0b6565c677dd9e24733077c9de97380eac
dataritz <gopalmiet@gmail.com>
2024-05-09 16:42:48 +0530

    jsgame

---
+++ js/navbarmain.js
@@ -0,0 +1,64 @@
+document.write(`<div class="container mt-1">
+<div class="row">
+  <div class="col">
+    <img src="image/logo.png" style="width: 100%;">
+  </div>
+</div>
+</div>
+<div class="container mt-1">
+<nav class="navbar navbar-expand-lg navbar-dark bg-primary" >
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#"><b>ICBMA-2024</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse"  id="navbarSupportedContent">
+      <ul class="navbar-nav ms-auto mb-2 mb-lg-0" >
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="index.html">ABOUT</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            COMMITTEES
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="advisory-board.html">ADVISORY BOARD</a></li>
+            <li><a class="dropdown-item" href="organising-committee.html">ORGANISING COMMITTEE</a></li>
+            <li><a class="dropdown-item" href="#">PROGRAM COMMITTEE</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            CALLS
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">CALL FOR PAPERS</a></li>
+            <li><a class="dropdown-item" href="#">SUBMISSION INSTRUCTIONS</a></li>
+          </ul>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#">REGISTRATION</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            PROGRAM
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">KEYNOTES & TUTORIAL</a></li>
+            <li><a class="dropdown-item" href="#">SCHEDULE</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            VENUE
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">LOCATION</a></li>
+            <li><a class="dropdown-item" href="#">HOW TO REACH</a></li>
+          </ul>
+        </li>
+      </ul>
+    </div>
+  </div>
+</nav>
+</div>`);
